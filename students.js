636474ba270e9e89c826f36a64f55de6ec304c4e
DharamTech <dharminderkumar0475@gmail.com>
2020-09-10 23:06:11 -0700

    common

---
+++ students.js
@@ -0,0 +1,65 @@
+const students = [{
+  "id": 1,
+  "first_name": "Hortensia",
+  "last_name": "Apark",
+  "email": "hapark0@berkeley.edu"
+},
+ {
+  "id": 2,
+  "first_name": "Haydon",
+  "last_name": "Stephens",
+  "email": "hstephens1@addtoany.com"
+},
+ {
+  "id": 3,
+  "first_name": "Si",
+  "last_name": "Adamik",
+  "email": "sadamik2@ezinearticles.com"
+},
+{
+  "id": 4,
+  "first_name": "Ewart",
+  "last_name": "O'Duggan",
+  "email": "eoduggan3@gnu.org"
+},
+{
+  "id": 5,
+  "first_name": "Latrina",
+  "last_name": "Byles",
+  "email": "lbyles4@vk.com"
+} ,
+{
+  "id": 6,
+  "first_name": "Alta",
+  "last_name": "Vallis",
+  "email": "avallis5@timesonline.co.uk"
+},
+{
+  "id": 7,
+  "first_name": "Ring",
+  "last_name": "Sawle",
+  "email": "rsawle6@123-reg.co.uk"
+},
+{
+  "id": 8,
+  "first_name": "Edgardo",
+  "last_name": "Lincke",
+  "email": "elincke7@archive.org"
+},
+{
+  "id": 9,
+  "first_name": "Carena",
+  "last_name": "Wimpress",
+  "email": "cwimpress8@bing.com"
+},
+{
+  "id": 10,
+  "first_name": "Dedie",
+  "last_name": "Robus",
+  "email": "drobus9@mlb.com"
+}]
+
+
+
+
+module.exports = students
